5dcb2e51b1250f966ef56ae3ae95c08ba0b4f124
Ben Rodarte <brodarte@digmap.com>
2020-12-01 14:09:54 -0800

    Add logging to feed routes

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -1,4 +1,5 @@
 import {Router, Request, Response} from 'express';
+import { v4 as uuid } from 'uuid';
 import {FeedItem} from '../models/FeedItem';
 import {NextFunction} from 'connect';
 import * as jwt from 'jsonwebtoken';
@@ -28,20 +29,26 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
 
 // Get all feed items
 router.get('/', async (req: Request, res: Response) => {
+  const reqId = uuid();
+  console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST STARTED: Get all feed items`);
   const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
   items.rows.map((item) => {
     if (item.url) {
       item.url = AWS.getGetSignedUrl(item.url);
     }
   });
+  console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST FINISHED: Get all feed items`);
   res.send(items);
 });
 
 // Get a feed resource
 router.get('/:id',
     async (req: Request, res: Response) => {
+      const reqId = uuid();
       const {id} = req.params;
+      console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST STARTED: Get feed item with id ${id}`);
       const item = await FeedItem.findByPk(id);
+      console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST FINISHED: Get feed item with id ${id}`);
       res.send(item);
     });
 
@@ -49,8 +56,11 @@ router.get('/:id',
 router.get('/signed-url/:fileName',
     requireAuth,
     async (req: Request, res: Response) => {
+      const reqId = uuid();
       const {fileName} = req.params;
+      console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST STARTED: Get signed url for ${fileName}`);
       const url = AWS.getPutSignedUrl(fileName);
+      console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST FINISHED: Get signed url for ${fileName}`);
       res.status(201).send({url: url});
     });
 
@@ -60,6 +70,8 @@ router.post('/',
     async (req: Request, res: Response) => {
       const caption = req.body.caption;
       const fileName = req.body.url; // same as S3 key name
+      const reqId = uuid();
+      console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST STARTED: Post new feed item ${fileName}`);
 
       if (!caption) {
         return res.status(400).send({message: 'Caption is required or malformed.'});
@@ -77,6 +89,7 @@ router.post('/',
       const savedItem = await item.save();
 
       savedItem.url = AWS.getGetSignedUrl(savedItem.url);
+      console.log(new Date().toLocaleString() + `: ${reqId} - REQUEST FINISHED: Post new feed item ${fileName}`);
       res.status(201).send(savedItem);
     });
 
